24b587e231689786fda568abe7650ddcc6816bb4
Noriki Ikeuchi <noriki.ikeuchi@ucl.meijo-u.ac.jp>
2019-07-13 12:48:46 +0900

    no message

---
+++ a/a/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  a
+//
+//  Created by Noriki Ikeuchi on 2019/04/29.
+//  Copyright © 2019 Noriki Ikeuchi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
