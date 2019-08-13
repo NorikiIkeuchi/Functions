3b3ceed7ef7ed7bc3c11728aa9765aba6e33857c
Noriki Ikeuchi <noriki.ikeuchi@ucl.meijo-u.ac.jp>
2019-08-13 13:49:48 +0900

    no message

--- Main/app/src/main/java/com/example/draw/main/Game1Activity.java
+++ Main/app/src/main/java/com/example/draw/main/Game1Activity.java
@@ -1,13 +1,8 @@
 package com.example.draw.main;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -88,42 +83,4 @@ public class Game1Activity extends AppCompatActivity {
 
     }
 
-
-
-    public static class MyView extends View {
-
-        Paint paint;
-
-        public MyView(Context context, AttributeSet attrs) {
-            super(context, attrs);
-            paint = new Paint();
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            // 背景、半透明
-            canvas.drawColor(Color.argb(127, 0, 127, 63));
-
-            // 円
-            paint.setColor(Color.argb(255, 68, 255, 255));
-            paint.setStrokeWidth(30);
-            paint.setAntiAlias(true);
-            paint.setStyle(Paint.Style.STROKE);
-            // (x1,y1,r,paint) 中心x1座標, 中心y1座標, r半径
-            canvas.drawCircle(450, 450, 100, paint);
-
-            // 矩形
-            paint.setColor(Color.argb(255, 255, 190, 0));
-            paint.setStrokeWidth(10);
-            paint.setStyle(Paint.Style.STROKE);
-            // (x1,y1,x2,y2,paint) 左上の座標(x1,y1), 右下の座標(x2,y2)
-            canvas.drawRect(480, 480, 850, 880, paint);
-
-            // 線
-            paint.setStrokeWidth(15);
-            paint.setColor(Color.argb(255, 0, 255, 120));
-            // (x1,y1,x2,y2,paint) 始点の座標(x1,y1), 終点の座標(x2,y2)
-            canvas.drawLine(350, 850, 750, 630, paint);
-        }
-    }
 }
