9bc9ce63fbedbecb0d81689f402b655642a483c5
Noriki Ikeuchi <noriki.ikeuchi@ucl.meijo-u.ac.jp>
2019-08-13 15:12:09 +0900

    ボタンクリックで画像をを最前面に移動させる機能の追加

--- Main/app/src/main/java/com/example/draw/main/Game1Activity.java
+++ Main/app/src/main/java/com/example/draw/main/Game1Activity.java
@@ -1,30 +1,34 @@
 package com.example.draw.main;
 
-<<<<<<< HEAD
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-=======
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.os.Bundle;
-import android.os.Handler;
-import android.util.AttributeSet;
->>>>>>> 4d5944ab948fbd2862938e88cab58416de86104f
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
+
 public class Game1Activity extends AppCompatActivity {
     TextView textTIme;
     ProgressBar pb;
     int time;
+    private TextView questionText;
+    private Button answerButton1;
+    private Button answerButton2;
+    private Button answerButton3;
+    private Button answerButton4;
+    private Button answerButton5;
+    private Button answerButton6;
+    private Button answerButton7;
+    private Button answerButton8;
+    private Button answerButton9;
+    private Button answerButton10;
+    private String questions[][] = new String[10][11];
 
 
     @Override
@@ -42,7 +46,7 @@ public class Game1Activity extends AppCompatActivity {
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                if(time <= 0) return;
+                if (time <= 0) return;
                 time--;
                 textTIme.setText(String.valueOf(time));
                 pb.setProgress(time);
@@ -60,79 +64,126 @@ public class Game1Activity extends AppCompatActivity {
         });
 
         handler.post(runnable);
+//
+//        Intent intent = getIntent();
+//        int nannido = intent.getIntExtra("難易度", 0);
+//
+//        TextView tv = findViewById(R.id.textView);
+//
+//
+//        switch (nannido) {
+//            case 10:
+//                tv.setText("中学一年生");
+//
+//                break;
+//            case 11:
+//                tv.setText("中学二年生");
+//                break;
+//            case 12:
+//                tv.setText("中学三年生");
+//                break;
+//            case 13:
+//                tv.setText("高校一年生");
+//                break;
+//            case 14:
+//                tv.setText("高校二年生");
+//                break;
+//            case 15:
+//                tv.setText("高校三年生");
+//                break;
+//            default:
+//                tv.setText("不正");
+//                break;
+//        }
 
-        Intent intent = getIntent();
-        int nannido = intent.getIntExtra("難易度", 0);
+        // ビューの初期化
+        questionText  = (TextView) findViewById(R.id.text_question);
+        answerButton1 = (Button) findViewById(R.id.button1);
+        answerButton2 = (Button) findViewById(R.id.button2);
+        answerButton3 = (Button) findViewById(R.id.button3);
+        answerButton4 = (Button) findViewById(R.id.button4);
+        answerButton5 = (Button) findViewById(R.id.button5);
+        answerButton6 = (Button) findViewById(R.id.button6);
+        answerButton7 = (Button) findViewById(R.id.button7);
+        answerButton8 = (Button) findViewById(R.id.button8);
+        answerButton9 = (Button) findViewById(R.id.button9);
+        answerButton10 = (Button) findViewById(R.id.button10);
 
-        TextView tv = findViewById(R.id.textView);
+        // 問題をセット
+        questions[0][0] = "テストの問題です";
+        questions[0][1] = "解答1";
+        questions[0][2] = "解答2";
+        questions[0][3] = "解答3";
+        questions[0][4] = "解答4";
+        questions[0][5] = "解答5";
+        questions[0][6] = "解答6";
+        questions[0][7] = "解答7";
+        questions[0][8] = "解答8";
+        questions[0][9] = "解答9";
+        questions[0][10] = "解答10";
+        // 問題をボタンのラベルとして表示
+        setQuizText();
 
+        //画像の定義
+        final ImageView vi = findViewById(R.id.imageView);
+        final ImageView vi2 = findViewById(R.id.imageView2);
 
-        switch (nannido){
-            case 10:
-                tv.setText("中学一年生");
+        //ボタンクリックで画像を最前面に移動
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vi.bringToFront();
+            }
+        });
 
-                break;
-            case 11:
-                tv.setText("中学二年生");
-                break;
-            case 12:
-                tv.setText("中学三年生");
-                break;
-            case 13:
-                tv.setText("高校一年生");
-                break;
-            case 14:
-                tv.setText("高校二年生");
-                break;
-            case 15:
-                tv.setText("高校三年生");
-                break;
-            default:
-                tv.setText("不正");
-                break;
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vi2.bringToFront();
+            }
+        });
+    }
+
+    //ランダムに配列を生成するメソッド
+    private int[] createRandomArray(int n, int offset){
+        int data[] = new int[n];
+        Random random1 = new Random();
+        Random random2 = new Random();
+        int buf, i, rnd1, rnd2;
+        for (i = 0; i < n; i++) {
+            data[i] = i + offset;
         }
+        for (i = 0; i < n * 10; i++) {
+            rnd1 = random1.nextInt(n);
+            rnd2 = random2.nextInt(n);
+            buf = data[rnd1];
+            data[rnd1] = data[rnd2];
+            data[rnd2] = buf;
+        }
+        return data;
+    }
+
+    //ボタンに問題をセットするメソッド
+    private void setQuizText(){
+        int rndNum[] = createRandomArray(10, 1);
+        //問題をセット
+        questionText.setText(questions[0][0]);
+        //選択肢をセット
+        answerButton1.setText(questions[0][rndNum[0]]);
+        answerButton2.setText(questions[0][rndNum[1]]);
+        answerButton3.setText(questions[0][rndNum[2]]);
+        answerButton4.setText(questions[0][rndNum[3]]);
+        answerButton5.setText(questions[0][rndNum[4]]);
+        answerButton6.setText(questions[0][rndNum[5]]);
+        answerButton7.setText(questions[0][rndNum[6]]);
+        answerButton8.setText(questions[0][rndNum[7]]);
+        answerButton9.setText(questions[0][rndNum[8]]);
+        answerButton10.setText(questions[0][rndNum[9]]);
+
 
     }
 
-<<<<<<< HEAD
-=======
 
-
-    public static class MyView extends View {
-
-        Paint paint;
-
-        public MyView(Context context, AttributeSet attrs) {
-            super(context, attrs);
-            paint = new Paint();
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            // 背景、半透明
-            canvas.drawColor(Color.argb(127, 0, 127, 63));
-
-            // 円
-            paint.setColor(Color.argb(255, 68, 255, 255));
-            paint.setStrokeWidth(30);
-            paint.setAntiAlias(true);
-            paint.setStyle(Paint.Style.STROKE);
-            // (x1,y1,r,paint) 中心x1座標, 中心y1座標, r半径
-            canvas.drawCircle(450, 450, 100, paint);
-
-            // 矩形
-            paint.setColor(Color.argb(255, 255, 190, 0));
-            paint.setStrokeWidth(10);
-            paint.setStyle(Paint.Style.STROKE);
-            // (x1,y1,x2,y2,paint) 左上の座標(x1,y1), 右下の座標(x2,y2)
-            canvas.drawRect(480, 480, 850, 880, paint);
-
-            // 線
-            paint.setStrokeWidth(15);
-            paint.setColor(Color.argb(255, 0, 255, 120));
-            // (x1,y1,x2,y2,paint) 始点の座標(x1,y1), 終点の座標(x2,y2)
-            canvas.drawLine(350, 850, 750, 630, paint);
-        }
-    }
->>>>>>> 4d5944ab948fbd2862938e88cab58416de86104f
 }
