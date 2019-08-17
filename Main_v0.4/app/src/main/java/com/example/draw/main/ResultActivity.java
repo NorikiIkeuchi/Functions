e2daef081e3eba32b5a8a3f496f5c32894c7b596
kamiyatm <deltaforce1102@gmail.com>
2019-08-18 01:19:45 +0900

    関数グラフプロットを組みこんだ

---
+++ Main_v0.4/app/src/main/java/com/example/draw/main/ResultActivity.java
@@ -0,0 +1,36 @@
+package com.example.draw.main;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        //Game1Activity.javaから得点を受け取り、表示する
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            int point = bundle.getInt("point");
+            TextView scoreText = (TextView) findViewById(R.id.textView_RPoint);
+            scoreText.setText(String.format("%d", point));
+        }
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ResultActivity.this, RankingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
