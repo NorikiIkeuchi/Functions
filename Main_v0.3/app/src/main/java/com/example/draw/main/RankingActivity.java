fb853df2508c2012c4d2f1007f2328f3ba0fbd52
Noriki Ikeuchi <noriki.ikeuchi@ucl.meijo-u.ac.jp>
2019-08-17 16:14:37 +0900

    RankingActiviyの追加

---
+++ Main_v0.3/app/src/main/java/com/example/draw/main/RankingActivity.java
@@ -0,0 +1,28 @@
+package com.example.draw.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RankingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranking);
+
+        //start画面への遷移
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RankingActivity.this, SelectActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
